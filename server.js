6427bf2fa4a12bd85089af5af109b0967fc2d79c
Mirosław Filip <miroslaw.filip@schibsted.pl>
2016-01-04 11:43:35 +0100

    404 and 5xx error handling

--- server.js
+++ server.js
@@ -2,21 +2,45 @@ var express = require('express');
 var app = express();
 
 /**
+ * Router
+ */
+app.get('/', function(request, response) {
+    throw new Error('App logic is somehow broken. 500.');
+    response.send('hello world');
+});
+
+/**
  * Logger
  */
 var logger = function(request, response, next) {
     console.log('There is an incoming request ' + Date.now());
     next();
 };
-
-//app.use(logger);
+app.use(logger);
 
 /**
- * Router
+ * Router fallback
  */
-app.get('/', logger, function(request, response) {
-    response.end('hello world');
-});
+var routerErrorHandler = function(req, res, next) {
+    var err = new Error('Route not found');
+    err.status = 404;
+    next(err);
+};
+app.use(routerErrorHandler);
+
+/**
+ * Global error handler
+ */
+var errorHandler = function(err, req, res, next) {
+    console.error(err.stack);
+    var status = err.status || 500;
+    res.status(status);
+    res.json({
+        message: 'Error handler fired',
+        error: (process.env.NODE_ENV === 'production') ? {} : err.toString()
+    });
+};
+app.use(errorHandler);
 
 var server = app.listen(3000, function() {
     var host = server.address().address;
