43251fe5b63356dc7e4fb9f6e64efd0777712b54
Mirosław Filip <miroslaw.filip@schibsted.pl>
2016-01-05 10:54:38 +0100

    Split infrastructure code to base, staging, prod configs

--- infrastructure/prod.js
+++ infrastructure/prod.js
@@ -4,17 +4,18 @@ var heroin = require('heroin-js');
 var baseConfig = require('./base');
 var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
 
-var stagingConfig = {
-    name: 'book-inventory-nodejs-staging',
+var prodConfig = {
+    name: 'book-inventory-nodejs',
     config_vars: {
         MONGO_URI: 'mongodb://heroku_4x78mgjj:5c76vmaot6lpjfdubl2k6f1qc4@ds033285.mongolab.com:33285/heroku_4x78mgjj',
-        NODE_ENV: 'staging'
+        NODE_ENV: 'production'
     },
-    collaborators: [
+    collaborators: ['mateusz.cyburt@schibsted.pl',
+        'tomasz.racia@schibsted.pl',
         'miroslaw.filip@schibsted.pl']
 };
 
-var finalConfig = _.merge(baseConfig, stagingConfig);
+var finalConfig = _.merge(baseConfig, prodConfig);
 console.log("Applying config:");
 console.log(finalConfig);
 
